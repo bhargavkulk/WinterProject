af86eb624ebfc5f1e309b75153ec442c65e07a2a
Dhruva Raja <dhruvraja24@gmail.com>
2019-12-29 21:43:31 +0530

    add existing file

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform player;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+    public float speed = 2f;
+
+
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 direction = player.position - transform.position;
+        direction.Normalize();
+        movement = direction;
+
+        movePlayer(movement);
+    }
+
+    void movePlayer(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+    }
+}
