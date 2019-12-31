97b7d309d262370a93aee7ddeaaa7e3f9806fd7b
Dhruva Raja <dhruvraja24@gmail.com>
2019-12-31 18:19:02 +0530

    Adding new

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,9 +6,10 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
     float timer;
-    float SpawnInterval = 1f;
+    public float SpawnInterval = 5f;
     public float inRadius = 1;
     public float outRadius = 2;
+    public GameObject g;
 
     
     void FixedUpdate()
@@ -20,7 +21,8 @@ public class EnemySpawner : MonoBehaviour
             Vector2 pos = new Vector2(Random.Range(-10f, 10f), Random.Range(-10f, 10f));
             pos = pos.normalized;
             pos *= Random.Range(inRadius, outRadius);
-            Instantiate(enemyPrefab, pos, Quaternion.Euler(0f, 0f, Random.Range(-180f, 180f)));
+
+            g = Instantiate(enemyPrefab, pos, Quaternion.Euler(0f, 0f, Random.Range(-180f, 180f)));
         }
     }
 }
